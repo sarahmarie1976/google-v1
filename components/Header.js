820061fae571c8b7a7bc1b1cef33fe75b7140f21
Sarahmarie D Hollenstein <sholle7@gmail.com>
2022-06-26 03:01:03 -0500

    :sparkles: added header section

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import User from './User';
+
+export default function Header() {
+  return (
+    <header className='flex justify-between p-5 text-sm text-gray-700' >
+      <div className='flex space-x-4 item-center'>
+        <p className='link'>About</p>
+        <p className='link'>Store</p>
+      </div>
+      <div className='flex space-x-4 item-center'> 
+      <p className='link'>Gmail</p>
+      <p className='link'>Images</p>
+      <User />
+      
+      </div>
+    </header>
+  );
+}
