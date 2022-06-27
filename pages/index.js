53b2d665e041c05f345482c671ce4f12c8578f08
Sarahmarie D Hollenstein <sholle7@gmail.com>
2022-06-26 22:36:46 -0500

    :sparkles: created body section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import { SearchIcon, MicrophoneIcon } from '@heroicons/react/solid';
 
 import Header from '../components/Header';
 
@@ -11,19 +13,29 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       {/* Header */}
-        <Header />
-
+      <Header />
 
       {/* Body */}
-
-
+      <form className='flex flex-col items-center mt-60'>
+        <Image
+          width='300'
+          objectFit='cover'
+          height='100'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
+          alt=''
+        />
+        <div className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl'>
+          <SearchIcon className='h-5 text-gray-500 mr-3' />
+          <input type='text' className='flex-grow focus:outline-none' />
+          <MicrophoneIcon className='h-5' />
+        </div>
+        <div className='flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center  '>
+          <button className='btn'>Google Search</button>
+          <button className='btn'>I'm Feeling Lucky</button>
+        </div>
+      </form>
 
       {/* Footer */}
-
-
-
-
-
     </div>
   );
 }
